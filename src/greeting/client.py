a941ed64c8da75c12ca0b7c28a7578539ee54cdc
danila12315454 <danilaprig@gmail.com>
2024-03-14 14:36:21 +0100

    Task 3 done

--- src/greeting/client.py
+++ src/greeting/client.py
@@ -1,14 +1,41 @@
-from service_pb2 import GreetingRequest, Language
+from enum import Enum
+
 import grpc
+from service_pb2 import AddRequest, GreetingRequest, Language
 from service_pb2_grpc import GreetingsStub
 
 
-def main() -> None:
+class LanguageEnum(Enum):
+    RUSSIAN = "RUSSIAN"
+    ENGLISH = "ENGLISH"
+
+
+def generate_default_client() -> GreetingsStub:
     channel = grpc.insecure_channel("localhost:50051")
     client = GreetingsStub(channel)
-    request = GreetingRequest(name="Danila", language="RUSSIAN")
+    return client
 
-    print(client.Greet(request))
+
+class GreetingClient:
+    @staticmethod
+    def greet(
+        name: str,
+        language: LanguageEnum,
+        client: GreetingsStub = generate_default_client(),
+    ) -> str:
+        request = GreetingRequest(name=name, language=language.value)
+        return client.Greet(request).greeting
+
+    @staticmethod
+    def add(a: int, b: int, client: GreetingsStub = generate_default_client()) -> int:
+        request = AddRequest(a=a, b=b)
+        return client.Add(request).answer
+
+
+def main() -> None:
+    print(GreetingClient.greet(name="Danila", language=LanguageEnum.ENGLISH))
+
+    print(GreetingClient.add(a=5, b=3))
 
 
 if __name__ == "__main__":
--- src/greeting/service.py
+++ src/greeting/service.py
@@ -1,30 +1,26 @@
-from concurrent import futures
 import random
+from concurrent import futures
 
 import grpc
-
-from service_pb2 import (
-    GreetingRequest,
-    Language,
-    GreetingResponse,
-)
 import service_pb2_grpc
+from service_pb2 import (AddRequest, AddResponse, GreetingRequest,
+                         GreetingResponse, Language)
 
 
-class GreetingsServicer(
-    service_pb2_grpc.GreetingsServicer
-):
-    def Greet(self, request, context):
+class GreetingsServicer(service_pb2_grpc.GreetingsServicer):
+    def Greet(self, request: GreetingRequest, context) -> GreetingResponse:
         if request.language == Language.RUSSIAN:
             return GreetingResponse(greeting=f"Привет {request.name}!")
         if request.language == Language.ENGLISH:
             return GreetingResponse(greeting=f"Hello {request.name}!")
 
-def serve():
+    def Add(self, request: AddRequest, context) -> AddResponse:
+        return AddResponse(answer=request.a + request.b)
+
+
+def serve() -> None:
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    service_pb2_grpc.add_GreetingsServicer_to_server(
-        GreetingsServicer(), server
-    )
+    service_pb2_grpc.add_GreetingsServicer_to_server(GreetingsServicer(), server)
     server.add_insecure_port("[::]:50051")
     server.start()
     server.wait_for_termination()
